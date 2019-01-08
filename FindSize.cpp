1850e14ed2b7f54f57255cf292d8325c74136030
gourisnair <gouri.gopan@gmail.com>
2019-01-08 20:52:19 +0530

    cpp program to find the size of certain datatypes

---
+++ FindSize.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout << "Size of an integer is " << sizeof(int);
+  cout << "\nSize of a floating point number is " << sizeof(float);
+  cout << "\nSize of double is " << sizeof(double);
+  cout << "\nSize of a character is " << sizeof(char);
+  return 0;
+}
